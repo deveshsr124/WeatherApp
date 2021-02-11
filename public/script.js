b2d6bfe18eb9d6a69e447195c37ad9d007951b26
Devesh Srivastava <170165@mac.du.ac.in>
2021-02-11 14:40:18 +0530

    intial commit

---
+++ public/script.js
@@ -0,0 +1,41 @@
+
+const iconContainer=document.querySelector('.icon-container');
+const inputValue = document.getElementById('city-search');
+const button=document.querySelector('.submit-btn');
+const locationElement=document.querySelector('[data-location]');
+const statusElement=document.querySelector('[data-status]');
+const windElement=document.querySelector('[data-wind]');
+const tempElement=document.querySelector('[data-temperature]');
+const humidityElement=document.querySelector('[data-Humidity]');
+
+
+button.addEventListener('click',function getData(){
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=265ed39b19667fd477c97a158e012bfa&units=metric`)
+  .then(res=> {
+    return res.json()
+  })
+  .then((data)=>{
+    setWeatherData(data);
+  })
+  .catch(err=>console.log(err));
+  
+})
+
+function setWeatherData(data){
+ locationElement.textContent=inputValue.value;
+ statusElement.textContent=data.weather[0].description;
+ windElement.textContent=data.wind.speed;
+ tempElement.textContent=data.main.temp;
+ humidityElement.textContent=data.main.humidity;
+ iconContainer.innerHTML=`<img src=" http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+app.listen(3000, () => {
+  console.log("server started");
+});
