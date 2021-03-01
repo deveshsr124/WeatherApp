51d84d6e0007db1ed69479489c2a50d9591f9a38
Devesh Srivastava <deveshsr124@gmail.com>
2021-03-01 19:14:03 +0530

    small change

--- public/script.js
+++ public/script.js
@@ -10,7 +10,7 @@ const humidityElement=document.querySelector('[data-Humidity]');
 
 
 button.addEventListener('click',function getData(){
-  const api=`http://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=265ed39b19667fd477c97a158e012bfa&units=metric`;
+  const api=`https://mycors-proxy-devesh.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=265ed39b19667fd477c97a158e012bfa&units=metric`;
   fetch(api)
   .then(res=> {
     return res.json()
